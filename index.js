f1c4aeedf0538f855a5389faeb9fbb195d49dd1b
marcosdima <marcosdimatteo.dev@gmail.com>
2024-03-14 10:23:54 -0300

    feat: save user

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 let mongoose = require('mongoose')
+let bodyParser = require('body-parser');
 require('dotenv').config()
 mongoose.connect(process.env.MONGO_URI);
+const Schema = mongoose.Schema;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
 app.use(cors())
 app.use(express.static('public'))
@@ -12,10 +17,6 @@ app.get('/', (req, res) => {
 });
 
 const exerciseSchema = mongoose.Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
-    default: mongoose.Types.ObjectId // Utiliza mongoose.Types.ObjectId para generar un ObjectId aleatorio por defecto
-  },
   username: String,
   description: String,
   duration: Number,
@@ -23,18 +24,10 @@ const exerciseSchema = mongoose.Schema({
 });
 
 const userSchema = mongoose.Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
-    default: mongoose.Types.ObjectId // Utiliza mongoose.Types.ObjectId para generar un ObjectId aleatorio por defecto
-  },
   username: String
 });
 
 const logSchema = mongoose.Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
-    default: mongoose.Types.ObjectId // Utiliza mongoose.Types.ObjectId para generar un ObjectId aleatorio por defecto
-  },
   username: String,
   count: Number,
   log: [{
@@ -47,6 +40,24 @@ const logSchema = mongoose.Schema({
 const Excercise = mongoose.model('Excersice', exerciseSchema);
 const User = mongoose.model('User', userSchema);
 const Log = mongoose.model('Log', logSchema);
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+
+  if (!username) return res.json({ "err": "Invalid name..."});
+
+  let newUser = new User({ name: username });
+
+  newUser.save().then(data => {
+      res.json({
+        username: username,
+        _id: data._id.toString()
+      });
+    }).catch(err => {
+      console.error(err);
+      res.status(500).json({ "error": "An error occurred while saving the user." });
+    });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
